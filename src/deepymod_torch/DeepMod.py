2d371cea898a8396e3cdb7efc63d4dbc66975ba1
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-01-10 14:10:17 +0100

    Changed flow to allow for multiple training passes
    flow previously restricted to contain first and final pass only. Changed to while loop implementation to allow for many more loops, triggered by reporting of additional variable from threshold()

--- src/deepymod_torch/DeepMod.py
+++ src/deepymod_torch/DeepMod.py
@@ -42,19 +42,31 @@ def DeepMoD(data, target, network_config, library_config, optim_config):
 
     # Initiating
     network, coeff_vector_list, sparsity_mask_list = deepmod_init(network_config, library_config)
-
-    # Training of the network
-    time_deriv_list, theta, coeff_vector_list = train(data, target, network, coeff_vector_list, sparsity_mask_list, library_config, optim_config_internal)
-
-    # Thresholding
-    scaled_coeff_vector_list = [scaling(coeff_vector, theta, time_deriv) for coeff_vector, time_deriv in zip(coeff_vector_list, time_deriv_list)]
-    sparse_coeff_vector_list, sparsity_mask_list = zip(*[threshold(scaled_coeff_vector, coeff_vector) for scaled_coeff_vector, coeff_vector in zip(scaled_coeff_vector_list, coeff_vector_list)])
-
-    print(coeff_vector_list, sparse_coeff_vector_list, sparsity_mask_list)
-    print('Now running final cycle.')
-
-    # Final Training without L1 and with the sparsity pattern
-    optim_config_internal['lambda'] = 0
-    time_deriv_list, theta, coeff_vector_list = train(data, target, network, sparse_coeff_vector_list, sparsity_mask_list, library_config, optim_config_internal)
+    
+    Final = False
+    while True:
+        
+        if Final:
+            # Final Training without L1 and with the accepted sparsity pattern
+            print(coeff_vector_list, sparse_coeff_vector_list, sparsity_mask_list)
+            print('Now running final cycle.')
+            
+            optim_config_internal['lambda'] = 0
+        
+        # Training of the network
+        time_deriv_list, theta, coeff_vector_list = train(data, target, network, coeff_vector_list, sparsity_mask_list, library_config, optim_config_internal)
+        
+        if Final:
+            break
+        
+        # Thresholding
+        scaled_coeff_vector_list = [scaling(coeff_vector, theta, time_deriv) for coeff_vector, time_deriv in zip(coeff_vector_list, time_deriv_list)]
+        sparse_coeff_vector_list, sparsity_mask_list, Overode_list = zip(*[threshold(scaled_coeff_vector, coeff_vector) for scaled_coeff_vector, coeff_vector in zip(scaled_coeff_vector_list, coeff_vector_list)])
+        
+        Final = True
+        for Overode_Response in Overode_list:
+            if Overode_Response:
+                Final = False
+                break
 
     return coeff_vector_list, sparsity_mask_list, network
