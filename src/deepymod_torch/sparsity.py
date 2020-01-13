da70fd86746d7ca9625d29bae2480ceb8f259e84
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-01-13 16:28:56 +0100

    added intelligence for zeroth and 1st order derivative common sense
    -check_need_overide now checks the zeroth and first derivatives are present as necessary
    -the higher flow avoids delaying teh final pass if only minimum zeroth and first derivatives remain. Also rejects the removal of these in case that diff_order already specified as 1.

--- src/deepymod_torch/sparsity.py
+++ src/deepymod_torch/sparsity.py
@@ -51,11 +51,16 @@ def threshold(scaled_coeff_vector, coeff_vector, sparsity_mask, library_config):
     sparse_coeff_vector = torch.where(torch.abs(scaled_coeff_vector) > torch.std(scaled_coeff_vector, dim=0), coeff_vector, torch.zeros_like(scaled_coeff_vector))
     Indices_To_Keep = torch.nonzero(sparse_coeff_vector)[:, 0]
     Overode = False
-    if library_config.get('Input_Type', None) == ('Strain' or 'Stress'):
+    if library_config.get('input_type', None) == ('Strain' or 'Stress'):        
         sparsity_mask_trial = sparsity_mask[Indices_To_Keep]
-        if Check_Need_Overide(sparsity_mask_trial, library_config['diff_order']):
-            Indices_To_Keep = Remove_High_Order(sparse_coeff_vector)
-            Overode = True
+        if check_need_overide(sparsity_mask_trial, library_config['diff_order']):
+            Indices_To_Keep = remove_high_order(sparse_coeff_vector)
+            if len(Indices_To_Keep) == 3:
+                print('Defaulted to minimum library size')
+            elif len(Indices_To_Keep) < 3:#Only possible to trigger if 'diff_order: 1' used and DeepMoD wants to remove a term.
+                Indices_To_Keep = torch.arange(3)
+            else:
+                Overode = True
                 
     sparsity_mask = sparsity_mask[Indices_To_Keep].detach()  # detach it so it doesn't get optimized and throws an error
     sparse_coeff_vector = sparse_coeff_vector[Indices_To_Keep].clone().detach().requires_grad_(True)  # so it can be optimized
@@ -63,14 +68,19 @@ def threshold(scaled_coeff_vector, coeff_vector, sparsity_mask, library_config):
     return sparse_coeff_vector, sparsity_mask, Overode
 
         
-def Check_Need_Overide(sparsity_mask_trial, original_diff_order):
+def check_need_overide(sparsity_mask_trial, original_diff_order):
     Index_Gap = original_diff_order+1
-        
-    #First check if all pairs are like
+    
+    #First check zeroth and first derivatives are present. In no model is there ever the situation where either of these should  be removed, except for a single spring
+    for coeff_index in [0, Index_Gap-1, Index_Gap]:
+        if not coeff_index in sparsity_mask_trial:
+            return True
+    
+    #Next check if all pairs are like
     for coeff_index in range(1, original_diff_order):
         if (coeff_index in sparsity_mask_trial) != (coeff_index+Index_Gap in sparsity_mask_trial):
             return True
-        
+    
     #Next check that no derivatives skipped
     Expected_State = False
     for coeff_index in range(1, original_diff_order):
@@ -83,7 +93,7 @@ def Check_Need_Overide(sparsity_mask_trial, original_diff_order):
     return False
 
 
-def Remove_High_Order(sparse_coeff_vector):
+def remove_high_order(sparse_coeff_vector):
     
     Number_Of_Coeffs = len(sparse_coeff_vector)
     Low_Index = (Number_Of_Coeffs // 2) - 1
