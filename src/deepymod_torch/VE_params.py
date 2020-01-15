81d1d75c8b64cdd441fa1dcbce581a40ae628ec5
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-01-15 11:08:41 +0100

    Create VE_params.py
    set of functions for taking result of DeepMod and using these coeff values to deduce the model parameters that give rise to them
    
    -also presnet is function for getting expected coeffs from model parameters.

---
+++ src/deepymod_torch/VE_params.py
@@ -0,0 +1,78 @@
+import sympy as sym
+import torch
+
+
+def model_params_from_coeffs(coeff_vector, print_expressions=False):
+    
+    coeff_expression_list, model_params_mask_list = kelvin_coeff_expressions(len(coeff_vector))
+    
+    if print_expressions:
+        print(coeff_expression_list)
+    
+    coeff_equations_list = [coeff_expression - coeff_value for coeff_expression, coeff_value in zip(coeff_expression_list, coeff_vector)]
+    
+    model_params_value_list = sym.solve(coeff_equations_list, model_params_mask_list)
+    
+    if len(model_params_value_list) == 0:
+        print('No solution possible for coefficient values and model complexity arrived at.')
+    
+    return model_params_value_list, model_params_mask_list
+
+
+def coeffs_from_model_params(E_mod_list, visc_list):
+    
+    coeff_count = len(E_mod_list)*2 - 1
+    
+    coeff_expression_list, model_params_mask_list = kelvin_coeff_expressions(coeff_count)
+    
+    coeff_value_list = [coeff_expression.subs(zip(model_params_mask_list, E_mod_list+visc_list)) for coeff_expression in coeff_expression_list]
+    
+    return coeff_value_list
+
+
+def kelvin_coeff_expressions(coeff_count):
+    
+    order = coeff_count // 2
+    
+    eps, sig = sym.symbols('epsilon,sigma', real=True)
+    E_0 = sym.symbols('E_0', real=True, negative=False)
+    
+    #Create and organise Kelvin general equation
+    summation, model_params_list = kelvin_sym_sum(order)
+    model_params_list = [E_0] + model_params_list
+    RHS = (1/E_0 + summation)*sig
+    RHS = sym.together(RHS)
+    RHS, denom = sym.fraction(RHS)
+    
+    full_expression = RHS - eps*denom
+    
+    #find coeffs - prep
+    expanded = sym.expand(full_expression)
+    dt = sym.symbols('dt')
+    
+    #Strain coeffs
+    coeff_expressions_list = [-expanded.coeff(eps, 1).coeff(dt, 0)]
+    for strain_order in range(2, order+1):
+        coeff_expressions_list += [-expanded.coeff(eps, 1).coeff(dt, strain_order)]
+        
+    #Stress coeffs
+    for stress_order in range(order+1):
+        coeff_expressions_list += [expanded.coeff(sig, 1).coeff(dt, stress_order)]
+    
+    return coeff_expressions_list, model_params_list
+
+
+def kelvin_sym_sum(order):
+    
+    E_Syms = [sym.symbols('E_'+str(Branch_Index), real=True, negative=False) for Branch_Index in range(1, order+1)]
+    Eta_Syms = [sym.symbols('eta_'+str(Branch_Index), real=True, negative=False) for Branch_Index in range(1, order+1)]
+    
+    all_syms = E_Syms + Eta_Syms
+    
+    dt = sym.symbols('dt')
+    
+    Expression = sym.S(0)
+    for Branch_Index in range(len(E_Syms)):
+        Expression += 1/(E_Syms[Branch_Index] + Eta_Syms[Branch_Index]*dt)
+        
+    return Expression, all_syms
