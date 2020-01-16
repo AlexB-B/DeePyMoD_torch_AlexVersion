35ea29a2caf56f8ba83b2411a2824144d84f5488
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-01-16 18:40:07 +0100

    stuff

--- config/start_notebook.sh
+++ config/start_notebook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 container_name="deepymod_pytorch"
-image="phimal/projects:general_v2"
+image="phimal/projects:general"
 
 #First automatically find project directory
 cd ../
--- src/deepymod_torch/.ipynb_checkpoints/neural_net-checkpoint.py
+++ src/deepymod_torch/.ipynb_checkpoints/neural_net-checkpoint.py
@@ -1,11 +1,34 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch
 
+from deepymod_torch.sparsity import scaling
+from torch.utils.tensorboard import SummaryWriter
+from deepymod_torch.tensorboard import custom_board
+
+
 def deepmod_init(network_config, library_config):
     '''
-    Builds a fully-connected NN according to specified parameters.
+    Constructs the neural network, trainable coefficient vectors and sparsity mask.
+
+    Parameters
+    ----------
+    network_config : dict
+        dict containing parameters for network construction. See DeepMoD docstring.
+    library_config : dict
+        dict containing parameters for library function. See DeepMoD docstring.
+
+    Returns
+    -------
+    torch_network: pytorch NN sequential module
+        The to-be-trained neural network.
+    coeff_vector_list: tensor list
+        list of coefficient vectors to be optimized, one for each equation.
+    sparsity_mask_list: tensor list
+        list of sparsity masks, one for each equation.
     '''
+
     # Building network
     input_dim = network_config['input_dim']
     hidden_dim = network_config['hidden_dim']
@@ -24,12 +47,15 @@ def deepmod_init(network_config, library_config):
     # Building coefficient vectors and sparsity_masks
     library_function = library_config['type']
 
-    sample_data = torch.ones(1, input_dim, requires_grad=True)  # we run a single forward pass to infer shapes
+    sample_data = torch.ones(1, input_dim, requires_grad=True)  # we run a single forward pass on fake data to infer shapes
     sample_prediction = torch_network(sample_data)
     _, theta = library_function(sample_data, sample_prediction, library_config)
     total_terms = theta.shape[1]
-
+    
     coeff_vector_list = [torch.randn((total_terms, 1), dtype=torch.float32, requires_grad=True) for _ in torch.arange(output_dim)]
+    if library_config.get('coeff_sign', None) == 'positive':
+        coeff_vector_list = [abs(tensor_for_output).detach().requires_grad_() for tensor_for_output in coeff_vector_list]
+    
     sparsity_mask_list = [torch.arange(total_terms) for _ in torch.arange(output_dim)]
 
     return torch_network, coeff_vector_list, sparsity_mask_list
@@ -37,34 +63,65 @@ def deepmod_init(network_config, library_config):
 
 def train(data, target, network, coeff_vector_list, sparsity_mask_list, library_config, optim_config):
     '''
-    Trains deepmod NN.
+    Trains the deepmod neural network and its coefficient vectors until maximum amount of iterations. Writes diagnostics to
+    runs/ directory which can be analyzed with tensorboard.
+    
+    Parameters
+    ----------
+    data : Tensor of size (N x M)
+        Coordinates corresponding to target data. First column must be time.
+    target : Tensor of size (N x L)
+        Data the NN is supposed to learn.
+    network : pytorch NN sequential module
+        Network to be trained.
+    coeff_vector_list : tensor list
+        List of coefficient vectors to be optimized
+    sparsity_mask_list : tensor list
+        List of sparsity masks applied to the library function.
+    library_config : dict
+        Dict containing parameters for the library function. See DeepMoD docstring.
+    optim_config : dict
+        Dict containing parameters for training. See DeepMoD docstring.
+    
+    Returns
+    -------
+    time_deriv_list : tensor list
+        list of the time derivatives after training.
+    theta : tensor
+        library matrix after training.
+    coeff_vector_list : tensor list
+        list of the trained coefficient vectors.
     '''
 
     max_iterations = optim_config['max_iterations']
     l1 = optim_config['lambda']
     library_function = library_config['type']
+    
+    optimizer = torch.optim.Adam([{'params': network.parameters(), 'lr': 0.001}, {'params': coeff_vector_list, 'lr': 0.001}])
 
-    optimizer = torch.optim.Adam([{'params': network.parameters(), 'lr': 0.002}, {'params': coeff_vector_list, 'lr': 0.002}])
-  
+    # preparing tensorboard writer
+    writer = SummaryWriter()
+    writer.add_custom_scalars(custom_board(coeff_vector_list))
+
     # Training
-    print('Epoch | Total loss | MSE | PI | L1 ')
+    print('Epoch | MSE loss ')
     for iteration in np.arange(max_iterations):
         # Calculating prediction and library
         prediction = network(data)
         time_deriv_list, theta = library_function(data, prediction, library_config)
         sparse_theta_list = [theta[:, sparsity_mask] for sparsity_mask in sparsity_mask_list]
-
+        
         # Scaling
         coeff_vector_scaled_list = [scaling(coeff_vector, sparse_theta, time_deriv) for time_deriv, sparse_theta, coeff_vector in zip(time_deriv_list, sparse_theta_list, coeff_vector_list)]
-
+        
         # Calculating PI
         reg_cost_list = torch.stack([torch.mean((time_deriv - sparse_theta @ coeff_vector)**2) for time_deriv, sparse_theta, coeff_vector in zip(time_deriv_list, sparse_theta_list, coeff_vector_list)])
         loss_reg = torch.sum(reg_cost_list)
-
+        
         # Calculating MSE
         MSE_cost_list = torch.mean((prediction - target)**2, dim=0)
         loss_MSE = torch.sum(MSE_cost_list)
-
+        
         # Calculating L1
         l1_cost_list = torch.stack([torch.sum(torch.abs(coeff_vector_scaled)) for coeff_vector_scaled in coeff_vector_scaled_list])
         loss_l1 = l1 * torch.sum(l1_cost_list)
@@ -77,10 +134,134 @@ def train(data, target, network, coeff_vector_list, sparsity_mask_list, library_
         loss.backward()
         optimizer.step()
 
+        # Tensorboard stuff
+        if iteration % 50 == 0:
+            writer.add_scalar('Total loss', loss, iteration)
+            for idx in np.arange(len(MSE_cost_list)):
+                # Costs
+                writer.add_scalar('MSE '+str(idx), MSE_cost_list[idx], iteration)
+                writer.add_scalar('Regression '+str(idx), reg_cost_list[idx], iteration)
+                writer.add_scalar('L1 '+str(idx), l1_cost_list[idx], iteration)
+
+                # Coefficients
+                for element_idx, element in enumerate(torch.unbind(coeff_vector_list[idx])):
+                    writer.add_scalar('coeff ' + str(idx) + ' ' + str(element_idx), element, iteration)
+
+                # Scaled coefficients
+                for element_idx, element in enumerate(torch.unbind(coeff_vector_scaled_list[idx])):
+                    writer.add_scalar('scaled_coeff ' + str(idx) + ' ' + str(element_idx), element, iteration)
+
         # Printing
         if iteration % 500 == 0:
             print(iteration, "%.1E" % loss.item(), "%.1E" % loss_MSE.item(), "%.1E" % loss_reg.item(), "%.1E" % loss_l1.item())
             for coeff_vector in zip(coeff_vector_list, coeff_vector_scaled_list):
                 print(coeff_vector[0])
 
-    return time_deriv_list, theta, coeff_vector_list
+    writer.close()
+    return time_deriv_list, sparse_theta_list, coeff_vector_list
+
+
+def train_group_mse(data, target, network, coeff_vector_list, optim_config):
+    '''
+    Trains the deepmod neural network and its coefficient vectors until maximum amount of iterations. Writes diagnostics to
+    runs/ directory which can be analyzed with tensorboard.
+    
+    Parameters
+    ----------
+    data : Tensor of size (N x M)
+        Coordinates corresponding to target data. First column must be time.
+    target : Tensor of size (N x L)
+        Data the NN is supposed to learn.
+    network : pytorch NN sequential module
+        Network to be trained.
+    optim_config : dict
+        Dict containing parameters for training. See DeepMoD docstring.
+    '''
+
+    max_iterations = optim_config['max_iterations']
+
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.001)
+
+    # preparing tensorboard writer
+    writer = SummaryWriter()
+    writer.add_custom_scalars(custom_board(coeff_vector_list))
+    
+    #preparing plot
+    fig, ax1 = plt.subplots()
+
+    ax1.set_xlabel('Time (s)')
+
+    colour = 'tab:blue'
+    ax1.tick_params(axis='y', labelcolor=colour)
+
+    ax2 = ax1.twinx()
+
+    colour = 'tab:red'
+    ax2.set_ylabel('target', color=colour)
+    ax2.plot(data.detach(), target, color=colour)
+    ax2.tick_params(axis='y', labelcolor=colour)
+    colour = 'tab:blue'
+    
+    plt.title('Current prediction ability of network')
+    
+    plt.ion()
+    
+
+    # Training
+    print('Epoch | Total loss | MSE | PI | L1 ')
+    for iteration in np.arange(max_iterations):
+        # Calculating prediction and library
+        prediction = network(data)
+        
+        #Update plot
+        ax1.clear()
+        ax1.set_ylabel('prediction', color=colour)
+        ax1.plot(data.detach(), prediction.detach(), color=colour)
+        align_yaxis(ax1, ax2) # Function not of my design.
+        fig.tight_layout()  # otherwise the right y-label is slightly clipped
+        fig.canvas.draw()
+        plt.show()
+
+        # Calculating MSE
+        MSE_cost_list = torch.mean((prediction - target)**2, dim=0)
+        loss_MSE = torch.sum(MSE_cost_list)
+
+        # Calculating total loss
+        loss = loss_MSE 
+
+        # Optimizer step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        # Tensorboard stuff
+        if iteration % 50 == 0:
+            writer.add_scalar('Total loss', loss, iteration)
+            for idx in np.arange(len(MSE_cost_list)):
+                # Costs
+                writer.add_scalar('MSE '+str(idx), MSE_cost_list[idx], iteration)
+          #      writer.add_scalar('L1 '+str(idx), l1_cost_list[idx], iteration)
+
+        if iteration % 500 == 0:
+            print(iteration, "%.1E" % loss.item())
+
+    writer.close()
+    return
+
+
+def align_yaxis(ax1, ax2):
+    """Align zeros of the two axes, zooming them out by same ratio"""
+    axes = (ax1, ax2)
+    extrema = [ax.get_ylim() for ax in axes]
+    tops = [extr[1] / (extr[1] - extr[0]) for extr in extrema]
+    # Ensure that plots (intervals) are ordered bottom to top:
+    if tops[0] > tops[1]:
+        axes, extrema, tops = [list(reversed(l)) for l in (axes, extrema, tops)]
+
+    # How much would the plot overflow if we kept current zoom levels?
+    tot_span = tops[1] + 1 - tops[0]
+
+    b_new_t = extrema[0][0] + tot_span * (extrema[0][1] - extrema[0][0])
+    t_new_b = extrema[1][1] - tot_span * (extrema[1][1] - extrema[1][0])
+    axes[0].set_ylim(extrema[0][0], b_new_t)
+    axes[1].set_ylim(t_new_b, extrema[1][1])
