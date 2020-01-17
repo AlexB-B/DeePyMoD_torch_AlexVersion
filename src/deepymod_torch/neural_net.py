50192e9a26774aae9eb46b47a0b1b44416579b02
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-01-17 19:22:42 +0100

    added adaptive learning rate support, dynamic plotting

--- src/deepymod_torch/neural_net.py
+++ src/deepymod_torch/neural_net.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch.nn as nn
 import torch
+from IPython import display
+import time
 
 from deepymod_torch.sparsity import scaling
 from torch.utils.tensorboard import SummaryWriter
@@ -97,14 +99,27 @@ def train(data, target, network, coeff_vector_list, sparsity_mask_list, library_
     l1 = optim_config['lambda']
     library_function = library_config['type']
     
-    optimizer = torch.optim.Adam([{'params': network.parameters(), 'lr': 0.001}, {'params': coeff_vector_list, 'lr': 0.001}])
-
+    optimizer_NN = torch.optim.Adam(network.parameters(), lr=0.01)
+    optimizer_coeffs = torch.optim.Adam(coeff_vector_list, lr=0.001)
+    scheduler_NN = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer_NN, factor=0.5, patience=100, cooldown=50)
+    # I am worried that the lr for the second parameter group (coeff_vector_list) starts off too high. But i do not want teh scheduler to make it small while the MSe has not yet been reduced. It may be necessary to make 2 seperate optimisers if I want 2 seperate schedulers....
+    
     # preparing tensorboard writer
     writer = SummaryWriter()
     writer.add_custom_scalars(custom_board(coeff_vector_list))
 
+    # preparing plot
+    fig, ax1 = plt.subplots()
+    plt.title('Current prediction ability of network')
+    ax1.set_xlabel('Time (s)')
+    colour = 'tab:blue'
+    ax1.set_ylabel('target', color=colour)
+    ax1.plot(data.detach(), target, color=colour)
+    ax1.tick_params(axis='y', labelcolor=colour)
+    ax2 = ax1.twinx()
+    ax2.tick_params(axis='y', labelcolor='tab:red')
+    
     # Training
-    print('Epoch | MSE loss ')
     for iteration in np.arange(max_iterations):
         # Calculating prediction and library
         prediction = network(data)
@@ -130,9 +145,12 @@ def train(data, target, network, coeff_vector_list, sparsity_mask_list, library_
         loss = loss_MSE + loss_reg + loss_l1
 
         # Optimizer step
-        optimizer.zero_grad()
+        optimizer_NN.zero_grad()
+        optimizer_coeffs.zero_grad()
         loss.backward()
-        optimizer.step()
+        optimizer_NN.step()
+        optimizer_coeffs.step()
+        scheduler_NN.step(loss)
 
         # Tensorboard stuff
         if iteration % 50 == 0:
@@ -152,12 +170,32 @@ def train(data, target, network, coeff_vector_list, sparsity_mask_list, library_
                     writer.add_scalar('scaled_coeff ' + str(idx) + ' ' + str(element_idx), element, iteration)
 
         # Printing
-        if iteration % 500 == 0:
+        if iteration % 200 == 0:
+            display.clear_output(wait=True)
+            
+            #Update plot
+            ax2.clear()
+            ax2.set_ylabel('prediction', color='tab:red')
+            ax2.plot(data.detach(), prediction.detach(), color='tab:red')
+            display.display(plt.gcf())
+            
+            print('Epoch | Total loss | MSE | PI | L1 ')
             print(iteration, "%.1E" % loss.item(), "%.1E" % loss_MSE.item(), "%.1E" % loss_reg.item(), "%.1E" % loss_l1.item())
             for coeff_vector in zip(coeff_vector_list, coeff_vector_scaled_list):
                 print(coeff_vector[0])
+            
+            print('lrs are', optimizer_NN.param_groups[0]['lr'], optimizer_coeffs.param_groups[0]['lr'])
 
     writer.close()
+    
+    display.clear_output(wait=False)
+    print('Epoch | Total loss | MSE | PI | L1 ')
+    print(iteration, "%.1E" % loss.item(), "%.1E" % loss_MSE.item(), "%.1E" % loss_reg.item(), "%.1E" % loss_l1.item())
+    for coeff_vector in zip(coeff_vector_list, coeff_vector_scaled_list):
+        print(coeff_vector[0])
+            
+    print('lrs are', optimizer_NN.param_groups[0]['lr'], optimizer_coeffs.param_groups[0]['lr'])
+            
     return time_deriv_list, sparse_theta_list, coeff_vector_list
 
 
@@ -180,8 +218,21 @@ def train_group_mse(data, target, network, coeff_vector_list, optim_config):
 
     max_iterations = optim_config['max_iterations']
 
-    optimizer = torch.optim.Adam(network.parameters(), lr=0.001)
-
+    optimizer = torch.optim.Adam(network.parameters(), lr=0.01) # Do not start with a lr as large as 0.1. 
+    #loss_threshold = 10**-3
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, factor=0.5, patience=50, threshold=0.0001, cooldown=50, eps=1e-08)
+    '''
+    an explanation of this scheduler
+    
+    What this is doing is it will reduce the learning rate if the improvement to the loss appears to be stagnating. This is to get the best of both worlds between a high learning rate (fast to remove early high loss and able to transition between different minima) and a low learning rate (able to find the optimum position with a minimum well)
+    
+    I have left lots of parameters explicitely stated, even if some are default, for transparency.
+    factor=0.5 (not default) means when the lr is adjusted, it is halved
+    patience=50 (not default) and threshold=0.0001 (default) must be discussed together. As the loss decreases the best (probably most recent at first) loss is remembered. As the improvement to loss slows, it may get to a point where, after a certain loss, none of the next patience=50 epochs achieve a loss better than this loss*(1-threshold=0.0001). Once this improvement has slowed down to this extent, a lr adjustment occurs. Essentially, the network as 50 epochs to reduce loss by 0.01% at some point, or else. This is only considered after the cooldown phase. Both increasing patience or decreasing threshold mean we are asking the loss improvment to have slowed down to a greater extent before adjustment.
+    cooldown=50 (not default) means after a lr adjustment, the network is allowed to settle for 50 epochs before another adjustment is considered. This is to allow the network to have a small lag phase before it improves without adjustments being too aggressive, and also just reduces the frequency of adjustment.
+    eps=1e-08 (default) means that very fine adjustments to lr are not bothered with. Considering the initial lr and factor, this amounts to a minimum of deterministic magnitude.
+    '''
+    
     # preparing tensorboard writer
     writer = SummaryWriter()
     writer.add_custom_scalars(custom_board(coeff_vector_list))
@@ -189,38 +240,22 @@ def train_group_mse(data, target, network, coeff_vector_list, optim_config):
     #preparing plot
     fig, ax1 = plt.subplots()
 
+    plt.title('Current prediction ability of network')
+    
     ax1.set_xlabel('Time (s)')
 
     colour = 'tab:blue'
+    ax1.set_ylabel('target', color=colour)
+    ax1.plot(data.detach(), target, color=colour)
     ax1.tick_params(axis='y', labelcolor=colour)
-
+    
     ax2 = ax1.twinx()
-
-    colour = 'tab:red'
-    ax2.set_ylabel('target', color=colour)
-    ax2.plot(data.detach(), target, color=colour)
-    ax2.tick_params(axis='y', labelcolor=colour)
-    colour = 'tab:blue'
+    ax2.tick_params(axis='y', labelcolor='tab:red')
     
-    plt.title('Current prediction ability of network')
-    
-    plt.ion()
-    
-
     # Training
-    print('Epoch | Total loss | MSE | PI | L1 ')
     for iteration in np.arange(max_iterations):
         # Calculating prediction and library
         prediction = network(data)
-        
-        #Update plot
-        ax1.clear()
-        ax1.set_ylabel('prediction', color=colour)
-        ax1.plot(data.detach(), prediction.detach(), color=colour)
-        align_yaxis(ax1, ax2) # Function not of my design.
-        fig.tight_layout()  # otherwise the right y-label is slightly clipped
-        fig.canvas.draw()
-        plt.show()
 
         # Calculating MSE
         MSE_cost_list = torch.mean((prediction - target)**2, dim=0)
@@ -233,6 +268,15 @@ def train_group_mse(data, target, network, coeff_vector_list, optim_config):
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        scheduler.step(loss)
+        
+        '''
+        if loss < loss_threshold:
+            optimizer.param_groups[0]['lr'] *= 0.1
+            loss_threshold *= 0.1
+            #Notice the [0]. This is because if the optimiser has many param groups (in DeepMoD it has 2, 1 for network params and 1 for coeff_vector) so the
+            #attribute called optimiser.param_groups is first a list, which contains dictionaries.
+        '''
         
         # Tensorboard stuff
         if iteration % 50 == 0:
@@ -240,28 +284,25 @@ def train_group_mse(data, target, network, coeff_vector_list, optim_config):
             for idx in np.arange(len(MSE_cost_list)):
                 # Costs
                 writer.add_scalar('MSE '+str(idx), MSE_cost_list[idx], iteration)
-          #      writer.add_scalar('L1 '+str(idx), l1_cost_list[idx], iteration)
+                #writer.add_scalar('L1 '+str(idx), l1_cost_list[idx], iteration)
 
-        if iteration % 500 == 0:
+        if iteration % 200 == 0:
+            display.clear_output(wait=True)
+            
+            #Update plot
+            ax2.clear()
+            ax2.set_ylabel('prediction', color='tab:red')
+            ax2.plot(data.detach(), prediction.detach(), color='tab:red')
+            display.display(plt.gcf())
+            
+            print('Epoch | MSE loss ')
             print(iteration, "%.1E" % loss.item())
+            print('lr is ', optimizer.param_groups[0]['lr'])
 
     writer.close()
+    display.clear_output(wait=False)
+    print('Epoch | MSE loss ')
+    print(iteration, "%.1E" % loss.item())
+    print('lr is ', optimizer.param_groups[0]['lr'])
     return
 
-
-def align_yaxis(ax1, ax2):
-    """Align zeros of the two axes, zooming them out by same ratio"""
-    axes = (ax1, ax2)
-    extrema = [ax.get_ylim() for ax in axes]
-    tops = [extr[1] / (extr[1] - extr[0]) for extr in extrema]
-    # Ensure that plots (intervals) are ordered bottom to top:
-    if tops[0] > tops[1]:
-        axes, extrema, tops = [list(reversed(l)) for l in (axes, extrema, tops)]
-
-    # How much would the plot overflow if we kept current zoom levels?
-    tot_span = tops[1] + 1 - tops[0]
-
-    b_new_t = extrema[0][0] + tot_span * (extrema[0][1] - extrema[0][0])
-    t_new_b = extrema[1][1] - tot_span * (extrema[1][1] - extrema[1][0])
-    axes[0].set_ylim(extrema[0][0], b_new_t)
-    axes[1].set_ylim(t_new_b, extrema[1][1])
