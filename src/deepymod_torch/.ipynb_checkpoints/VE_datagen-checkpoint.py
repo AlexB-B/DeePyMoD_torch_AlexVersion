96393656193c1268c6ce332ef08b0729bd241896
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-02-10 16:08:49 +0100

    maxwell kelvin revelation
    added comments explaining that data generated interprets parameters as being part of different models depending on context
    removed _alt functions
    validation now gets coeffs from realising model params were interpreted in the context of the correct model depending on given input_type

--- src/deepymod_torch/.ipynb_checkpoints/VE_datagen-checkpoint.py
+++ src/deepymod_torch/.ipynb_checkpoints/VE_datagen-checkpoint.py
@@ -17,7 +17,10 @@ def calculate_strain_stress(input_type, time_array, input_expr, E_mods, viscs, D
         input_lambda = sym.lambdify(t, input_expr)
         D_input_lambda = sym.lambdify(t, D_input_expr)
     
-    relax_creep_lambda = relax_creep_kelvin(E_mods, viscs, input_type)
+    # The following function interprets the provided model parameters differently depending on the input_type.
+    # If the input_type is 'Strain' then the parameters are assumed to refer to a Maxwell model, whereas
+    # if the input_type is 'Stress' then the parameters are assumed to refer to a Kelvin model.
+    relax_creep_lambda = relax_creep(E_mods, viscs, input_type)
     
     if relax_creep_lambda == False:
         return False, False
@@ -48,7 +51,12 @@ def calculate_strain_stress(input_type, time_array, input_expr, E_mods, viscs, D
     return strain_array, stress_array
 
 
-def relax_creep_kelvin(E_mods, viscs, input_type):
+def relax_creep(E_mods, viscs, input_type):
+    
+    # The following function interprets the provided model parameters differently depending on the input_type.
+    # If the input_type is 'Strain' then the parameters are assumed to refer to a Maxwell model, whereas
+    # if the input_type is 'Stress' then the parameters are assumed to refer to a Kelvin model.
+    # The equations used thus allow the data to be generated according to the model now designated.
     
     E_mods_1plus_array = np.array(E_mods[1:]).reshape(-1,1)
     viscs_array = np.array(viscs).reshape(-1,1)
@@ -66,61 +74,8 @@ def relax_creep_kelvin(E_mods, viscs, input_type):
     return relax_creep_lambda
 
 
-def calculate_strain_stress_alt(input_type, time_array, input_lambda, E_mods, viscs):
-    
-    relax_creep_lambda, D_relax_creep_lambda = relax_creep_kelvin_alt(E_mods, viscs, input_type)
-    
-    if relax_creep_lambda == False:
-        return False, False
-    
-    start_time_point = time_array[0]
-    
-    integrand_lambda = lambda x, t: D_relax_creep_lambda(t-x)*input_lambda(x)
-    integral_lambda = lambda t: integ.quad(integrand_lambda, start_time_point, t, args=(t))[0]
-    
-    output_array = np.array([])
-    input_array = np.array([])
-    for time_point in time_array:
-        first_term = input_lambda(start_time_point)*relax_creep_lambda(time_point)#-start_time_point)
-        second_term = integral_lambda(time_point)
-        output_array = np.append(output_array, first_term + second_term)
-        input_array = np.append(input_array, input_lambda(time_point))
-    
-    if input_type == 'Strain':
-        strain_array = input_array
-        stress_array = output_array
-    else:
-        strain_array = output_array
-        stress_array = input_array
-        
-    strain_array = strain_array.reshape(time_array.shape)
-    stress_array = stress_array.reshape(time_array.shape)
-    
-    return strain_array, stress_array
-
-
-def relax_creep_kelvin_alt(E_mods, viscs, input_type):
-    
-    E_mods_1plus_array = np.array(E_mods[1:]).reshape(-1,1)
-    viscs_array = np.array(viscs).reshape(-1,1)
-    
-    taus = viscs_array/E_mods_1plus_array
-    
-    if input_type == 'Strain':
-        relax_creep_lambda = lambda t: E_mods[0] + np.sum(np.exp(-t/taus)*E_mods_1plus_array)
-        D_relax_creep_lambda = lambda t: -np.sum(np.exp(-t/taus)*E_mods_1plus_array/taus)
-    elif input_type == 'Stress':
-        relax_creep_lambda = lambda t: 1/E_mods[0] + np.sum((1-np.exp(-t/taus))/E_mods_1plus_array)
-        D_relax_creep_lambda = lambda t: np.sum(np.exp(-t/taus)/(E_mods_1plus_array*taus))
-    else:
-        print('Incorrect input_type')
-        relax_creep_lambda = False
-        D_relax_creep_lambda = False
-    
-    return relax_creep_lambda, D_relax_creep_lambda
-
 #Data Validation routines
-def equation_residuals(time_array, strain_array, stress_array, E_mods, viscs):
+def equation_residuals(time_array, strain_array, stress_array, E_mods, viscs, input_type):
     
     diff_order = len(viscs)
 
@@ -128,7 +83,14 @@ def equation_residuals(time_array, strain_array, stress_array, E_mods, viscs):
     stress_theta = num_derivs(stress_array, time_array, diff_order)
     num_theta = np.concatenate((strain_theta, stress_theta), axis=1)
     
-    coeffs = VE_params.coeffs_from_model_params(E_mods, viscs)
+    if input_type == 'Stress':
+        coeffs = VE_params.coeffs_from_model_params_kelvin(E_mods, viscs)
+    elif input_type == 'Strain':
+        coeffs = VE_params.coeffs_from_model_params_maxwell(E_mods, viscs)
+    else:
+        print('input_type is wrong')
+        return None
+    
     coeffs_strain_array = np.array([coeffs[0]] + [1] + coeffs[1:diff_order])
     coeffs_stress_array = np.array(coeffs[diff_order:])
     coeffs_array = np.concatenate((-coeffs_strain_array, coeffs_stress_array)).reshape(-1,1)
