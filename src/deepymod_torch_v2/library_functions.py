ce58a64c6760a617f0bbbdaefc0c96595db0da4e
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-05-13 17:41:35 +0200

    Complete transfer to DeepMoD v2
    MAJOR: Addition of mech_library() and auto_derivs(), mostly identical to legacy version in deepmod v1, but structure of parameters changed. Was 2 args then a dict. Now a 2-tuple then a series of kwargs which have been unpacked from a dict. ** used to pack kwargs back into familiar dict format.

--- src/deepymod_torch_v2/library_functions.py
+++ src/deepymod_torch_v2/library_functions.py
@@ -87,4 +87,62 @@ def library_2Din_1Dout(input, poly_order, diff_order):
         theta = torch.matmul(u[:, :, None], du[:, None, :]).view(samples,-1)
         
         return [u_t], theta
+
     
+def mech_library(inputs, **library_config):    
+    
+    prediction, data = inputs
+    
+    # Begin by computing the values of the terms corresponding to the input, for which an analytical expression is given.
+    # This only needs to be done for the very first epoch, after which the values are known and stored in the library_config dictionary.
+    # The first condition checks if input_theta was previously already calculated and saved.
+    # It also checks that the previous saved input_theta was not one that corresponded to a different data tensor.
+    if ('input_theta' in library_config) and (library_config['input_theta'].shape[0] == data.shape[0]):
+        input_theta = library_config['input_theta']
+    else:
+        input_data = library_config['input_expr'](data)
+        input_derivs = auto_deriv(data, input_data, library_config)
+        
+        input_data, input_derivs = input_data.detach(), input_derivs.detach()
+        
+        input_theta = torch.cat((input_data, input_derivs), dim=1)
+        library_config['input_theta'] = input_theta
+    
+    # Next use the result of the feedforward pass of the NN to calculate derivatives of your prediction with respect to time. 
+    output_derivs = auto_deriv(data, prediction, library_config)
+    output_theta = torch.cat((prediction, output_derivs), dim=1)
+    
+    # Next identify the input/output as Stress/Strain and organise into returned variables
+    input_type = library_config['input_type']
+    if input_type == 'Strain':
+        strain = input_theta
+        stress = output_theta
+    elif input_type == 'Stress':
+        strain = output_theta
+        stress = input_theta
+    else:
+        print('Improper description of input choice. Was: '+input_type+'. Should be either \'Strain\' or \'Stress\'')
+        
+    strain_t = strain[:, 1:2] # Extract the first time derivative of strain
+    strain = torch.cat((strain[:, 0:1], strain[:, 2:]), dim=1) # remove this before it gets put into theta
+    strain *= -1 # The coefficient of all strain terms will always be negative. rather than hoping deepmod will find these negative terms, we assume the negative factor here and later on DeepMoD will just find positive coefficients
+    theta = torch.cat((strain, stress), dim=1) # I have arbitrarily set the convention of making Strain the first columns of data
+    
+    return [strain_t], theta
+
+
+def auto_deriv(data, prediction, library_config):
+    '''
+    data and prediction must be single columned tensors.
+    If it is desired to calculate the derivatives of different predictions wrt different data, this function must be called multiple times.
+    This function does not return a column with the zeroth derivative (the prediction).
+    '''
+    max_order = library_config['diff_order']
+    
+    # First derivative builds off prediction.
+    derivs = grad(prediction, data, grad_outputs=torch.ones_like(prediction), create_graph=True)[0]
+    for _ in range(max_order-1):
+        # Higher derivatives chain derivatives from first derivative.
+        derivs = torch.cat((derivs, grad(derivs[:, -1:], data, grad_outputs=torch.ones_like(prediction), create_graph=True)[0]), dim=1)
+            
+    return derivs
