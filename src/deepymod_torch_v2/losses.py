08ce905ddf3a1de7b3c2fff64e41aec1c1af9ce0
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-05-13 17:43:11 +0200

    Complete transfer to DeepMoD v2
    name change for clarity in l1 loss
    MAJOR: Addition of negative aversion loss, unchanged from deepmod v1, except for trivial restructuring of code to match new style.

--- src/deepymod_torch_v2/losses.py
+++ src/deepymod_torch_v2/losses.py
@@ -1,6 +1,7 @@
 import torch
+from deepymod_torch_v2.sparsity import scaling # Why here?
+
 import torch.nn as nn
-from deepymod_torch.sparsity import scaling
 
 
 def reg_loss(time_deriv_list, sparse_theta_list, coeff_vector_list):
@@ -13,13 +14,15 @@ def mse_loss(prediction, target):
     loss = torch.mean((prediction - target)**2, dim=0)
     return loss
 
-def l1_loss(coeff_vector_list, l1):
+def l1_loss(scaled_coeff_vector_list, l1):
     '''Loss functions for the L1 loss on the coefficients. Calculates loss for each term in list.'''
-    loss = torch.stack([torch.sum(torch.abs(coeff_vector)) for coeff_vector in coeff_vector_list])
+    loss = torch.stack([torch.sum(torch.abs(coeff_vector)) for coeff_vector in scaled_coeff_vector_list])
     return l1 * loss
-   
     
-
+def na_loss(scaled_coeff_vector_list, kappa):
+    '''Loss functions for the negative aversion loss on the coefficients. Calculates loss for each term in list.'''
+    loss = torch.stack([torch.sum(nn.functional.relu(-coeff_vector))**2 for coeff_vector in scaled_coeff_vector_list])        
+    return kappa * loss
 
 
  
