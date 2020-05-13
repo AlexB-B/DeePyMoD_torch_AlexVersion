3bc2c2cd719409d1128ba416b167ddc83c343c1c
Alex Brandon-Bravo <alexanderbbravo@gmail.com>
2020-05-13 17:55:36 +0200

    Complete transfer to DeepMoD v2
    Changes to threshold() and threshold_single() to allow for custom threshold criteria to be set in dictionary, similar implementation to deepmod v1.

--- src/deepymod_torch_v2/sparsity.py
+++ src/deepymod_torch_v2/sparsity.py
@@ -16,19 +16,22 @@ def scaling(coeff_vector_list, sparse_theta_list, time_deriv_list):
     coeff_vector_scaled_list = [scaling_single_vec(coeff_vector, sparse_theta, time_deriv) for time_deriv, sparse_theta, coeff_vector in zip(time_deriv_list, sparse_theta_list, coeff_vector_list)]
     return coeff_vector_scaled_list
 
-def threshold_single(coeff_vector_scaled, coeff_vector):
+def threshold_single(coeff_vector_scaled, coeff_vector, optim_config):
     '''Removes coefficient if |value| < std(coefficient_vec) and returns new coefficient vector and sparsity mask. '''
-    sparse_coeff_vector = torch.where(torch.abs(coeff_vector_scaled) > torch.std(coeff_vector_scaled, dim=0), coeff_vector, torch.zeros_like(coeff_vector_scaled))
+    thresh_func = optim_config['thresh_func']
+    thresh_val = thresh_func(coeff_vector_scaled, coeff_vector, optim_config)
+    
+    sparse_coeff_vector = torch.where(torch.abs(coeff_vector_scaled) > thresh_val, coeff_vector, torch.zeros_like(coeff_vector_scaled))
     sparsity_mask = torch.nonzero(sparse_coeff_vector)[:, 0].detach()  # detach it so it doesn't get optimized and throws an error
     sparse_coeff_vector = torch.nn.Parameter(sparse_coeff_vector[sparsity_mask].clone().detach())
 
     return sparse_coeff_vector, sparsity_mask
     
-def threshold(coeff_vector_list, sparse_theta_list, time_deriv_list):
+def threshold(coeff_vector_list, sparse_theta_list, time_deriv_list, optim_config):
     '''Wrapper around threshold_single to threshold list of vectors. Also performs scaling.'''
 
     coeff_vector_scaled_list = scaling(coeff_vector_list, sparse_theta_list, time_deriv_list)
-    result = [threshold_single(coeff_vector_scaled, coeff_vector) for coeff_vector_scaled, coeff_vector in zip(coeff_vector_scaled_list, coeff_vector_list)]
+    result = [threshold_single(coeff_vector_scaled, coeff_vector, optim_config) for coeff_vector_scaled, coeff_vector in zip(coeff_vector_scaled_list, coeff_vector_list)]
     sparse_coeff_vector_list, sparsity_mask_list = map(list, zip(*result))
     
     return sparse_coeff_vector_list, sparsity_mask_list
